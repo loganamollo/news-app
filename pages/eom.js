6d9e1747fd19ecefc9ebb5e04cd194bd278f9300
loganamollo <loganamollo@google.com>
2022-02-07 12:04:22 +0300

    updated EOM page and style

--- pages/eom.js
+++ pages/eom.js
@@ -1,11 +1,26 @@
-export const EOM = () => {
+export const EOM = ({employee}) => {
     return(
         <div className="page-container">
             <h1>Employee of the month</h1>
+            <div>
+                <h3>{employee.name}</h3>
+                <p>{employee.position}</p>
+                <img src={employee.image} />
+            </div>
         </div>
     )
 }
 
- 
+export const getServerSideProps = async (pageContext) => {
+     const apiResponse = await fetch(
+         'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth'
+     )
+     const employee = await apiResponse.json()
+    return({
+        props: {
+            employee
+        }
+    })
+}
 
 export default EOM
